dc20bb50146e44e6b9c23afbc989ac0475f9ec80
Okan Binli <okanb3@gmail.com>
2015-08-17 15:34:59 +0300

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :topics
+  has_many :posts
+
+  has_attached_file :avatar
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+end
